d129159b24c9a6bbe5241d38e3dd4d2d6e1a5f05
Valeria Saparina <kzn.2003valery@gmail.com>
2019-04-12 20:55:45 +0300

    ...

---
+++ app/src/main/java/com/example/user/myapplication/API/Model.java
@@ -0,0 +1,12 @@
+package com.example.user.myapplication.API;
+
+public class Model {
+    protected String name;
+    protected String city;
+    protected String location;
+    protected String category;
+    protected String start_at;
+    protected String url;
+    protected String image;
+
+}
